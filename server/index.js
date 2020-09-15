8ca1e061965d5463146b2342d079963365e60e45
prateek <prateekjhawhiteteeth@gmail.com>
2020-09-16 02:27:23 +0530

    Added functionality of removing user when he/she quits

--- server/index.js
+++ server/index.js
@@ -37,7 +37,12 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        console.log('User had left ! !');
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+        }
     });
 });
 
